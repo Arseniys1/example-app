202fa86d606f4cfc27f3cadec395044a15e2408d
Arseniy <thevalakas1@gmail.com>
2025-09-04 18:29:38 +0500

    Упростил HandlesDateFilters.php

--- app/Traits/HandlesDateFilters.php
+++ app/Traits/HandlesDateFilters.php
@@ -6,41 +6,14 @@ use Illuminate\Contracts\Database\Query\Builder;
 
 trait HandlesDateFilters
 {
-    protected function parseDate(string $date): ?string
-    {
-        if (empty($date)) {
-            return null;
-        }
-
-        $formats = [
-            'Y-m-d H:i:s',
-            'Y-m-d',
-        ];
-
-        foreach ($formats as $format) {
-            $parsedDate = \DateTime::createFromFormat($format, $date);
-            if ($parsedDate !== false) {
-                return $parsedDate->format('Y-m-d');
-            }
-        }
-
-        return null;
-    }
-
     protected function applyDateFilters(Builder $query, array $filters, $dateField = 'created_at'): Builder
     {
         if (!empty($filters['dateFrom'])) {
-            $dateFrom = $this->parseDate($filters['dateFrom']);
-            if ($dateFrom) {
-                $query->where($dateField, '>=', $dateFrom);
-            }
+            $query->where($dateField, '>=', $filters['dateFrom']);
         }
 
         if (!empty($filters['dateTo'])) {
-            $dateTo = $this->parseDate($filters['dateTo']);
-            if ($dateTo) {
-                $query->where($dateField, '<=', $dateTo);
-            }
+            $query->where($dateField, '<=', $filters['dateTo']);
         }
 
         return $query;
